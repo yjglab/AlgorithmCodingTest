bb6c4b0d1284344f2b87f9321f2a952a391b0407
yjglab <yjgdesign@gmail.com>
2022-02-17 02:24:37 +0900

    떡볶이떡만들기

---
+++ Binary_search/떡볶에떡만들기.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+lst = list(map(int, input().split()))
+
+def bs(stt, end):
+    result = 0
+    while stt <= end:
+        mid = (stt + end) // 2
+        remain = sum([i - mid if i - mid > 0 else 0 for i in lst])
+        if remain > m:
+            stt = mid + 1
+        elif remain <= m:
+            result = mid
+            end = mid - 1
+    return result
+
+print(bs(min(lst), max(lst)))
