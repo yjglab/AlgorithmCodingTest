75263d13fc650a9b81ee396db58d489e6d58c034
yjglab <yjgdesign@gmail.com>
2022-02-18 03:31:10 +0900

    개미전사 풀이

--- Dynamic_programming/개미전사.py
+++ Dynamic_programming/개미전사.py
@@ -1,7 +1,7 @@
 n = int(input())
 foods = [0] + list(map(int, input().split()))
 # 1 3 1 5 3 4  1
-# 1 3 3 8 8 12 12
+# 1 3 3 8 8 12 12 
 d = [0] * (n + 1)
 d[1], d[2] = foods[1], max(d[1], foods[2])
 for i in range(n + 1):
