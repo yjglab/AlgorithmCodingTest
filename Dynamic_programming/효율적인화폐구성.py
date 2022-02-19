1060e8f06d52c48bcd5094b623cd1213a81026a2
yjglab <yjgdesign@gmail.com>
2022-02-20 02:06:06 +0900

    효율적인화폐구성 풀이 수정

--- Dynamic_programming/효율적인화폐구성.py
+++ Dynamic_programming/효율적인화폐구성.py
@@ -5,8 +5,8 @@ for _ in range(n):
 d = [10000] * 10001
 d[0] = 0
  
-for i in range(money[0], m + 1):
-    for k in money:
-        if d[i - k] != 10000:
-            d[i] = min(d[i], d[i - k] + 1)
+for i in range(n):
+    for j in range(money[i], m + 1):
+        if d[j - money[i]] != 10000:
+            d[j] = min(d[j], d[j - money[i]] + 1)
 print(d[m]) if d[m] != 10000 else -1
