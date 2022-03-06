8a46ce6b90c5bac6957977c32f0988030d65a6f6
yjglab <yjgdesign@gmail.com>
2022-03-06 13:58:30 +0900

    전보

---
+++ Dijkstra/전보.py
@@ -0,0 +1,34 @@
+import heapq
+
+q = []
+n, m, c = map(int, input().split())
+INF = int(1e9)
+graph = [[] for _ in range(n + 1)]
+
+distance = [INF] * (n + 1) 
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]:
+            continue
+        for i in graph[now]:
+            cost = i[1] + dist
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(c)
+cnt = 0
+result = 0
+for i in range(1, n + 1):
+    if distance[i] != INF:
+        cnt += 1
+    if distance[i] != INF and distance[i] > result:
+        result = distance[i]
+print(cnt - 1, result)
