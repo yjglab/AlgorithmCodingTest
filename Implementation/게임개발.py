305a8d9123d36a4a704ee08042674bc56544fa00
yjglab <yjgdesign@gmail.com>
2022-06-26 21:33:03 +0900

    try 2

--- Implementation/게임개발.py
+++ Implementation/게임개발.py
@@ -46,3 +46,35 @@ while True:
         print(a, b)
     
 print(cnt, a, b)
+
+# 2트
+n, m = map(int, input().split())
+move = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+x, y, d = map(int, input().split())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+arr[x][y] = 2 # 방문 표시
+d_cnt = 0
+res = 1
+while True:
+    d = (d + 3) % 4
+    nx, ny = x + move[d][0], y + move[d][1]
+    if arr[nx][ny] == 0:
+        d_cnt = 0
+        x, y = nx, ny
+        arr[x][y] = 2
+        res += 1
+    else:
+        d_cnt += 1
+    if d_cnt == 4:
+        
+        nd = (d + 2) % 4
+        nx, ny = x + move[nd][0], y + move[nd][1]
+        if arr[nx][ny] == 1:
+            break 
+        x, y = nx, ny
+        d_cnt = 0
+print(res)
