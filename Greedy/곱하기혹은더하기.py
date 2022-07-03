f904087df545b43f5f3a5e7932018a51944bec49
yjglab <yjgdesign@gmail.com>
2022-07-03 16:09:06 +0900

    try 2

--- Greedy/곱하기혹은더하기.py
+++ Greedy/곱하기혹은더하기.py
@@ -11,3 +11,15 @@ def solution(arr):
             result += arr[i]
     return result
 print(solution(arr))
+
+# try 2
+arr = list(map(int, input()))
+
+def solution(arr):
+    if len(arr) == 1:
+        return arr[0]
+    now = max(arr[0] + arr[1], arr[0] * arr[1])
+    for i in range(2, len(arr)):
+        now = max(now + arr[i], now * arr[i])
+    return now
+print(solution(arr))
