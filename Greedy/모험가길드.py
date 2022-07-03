06d48d3af3123683d48b1c32f65a7370ff29d7f4
yjglab <yjgdesign@gmail.com>
2022-07-03 15:58:52 +0900

    try 2

--- Greedy/모험가길드.py
+++ Greedy/모험가길드.py
@@ -13,3 +13,19 @@ def solution(n, arr):
     return result
 
 print(solution(n, arr))
+
+# try 2
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+# 3 2 2 2 1
+result, max_count = 0, 0
+group = []
+for i in range(n):
+    max_count = max(max_count, arr[i])
+    group.append(arr[i])
+    if len(group) == max_count:
+        max_count = 0
+        result += 1
+        group = []
+print(result)
