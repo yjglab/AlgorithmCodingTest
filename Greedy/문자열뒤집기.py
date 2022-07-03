1826aaf70b1a70a7548ddd8f028102a00d774a54
yjglab <yjgdesign@gmail.com>
2022-07-03 16:32:17 +0900

    try 2

--- Greedy/문자열뒤집기.py
+++ Greedy/문자열뒤집기.py
@@ -1,4 +1,4 @@
-def solution(s):
+def solution(s): # 오답일 듯
     c = 0
     if s.count(0) < s.count(1):
         c = 0
@@ -21,3 +21,18 @@ def solution(s):
 
 s = list(map(int, input()))
 print(solution(s))
+
+# try 2
+arr = [1, 0, 0, 1, 0, 0, 1, 1]
+arr = list(map(int, input()))
+def solution(target):
+    cnt = 0
+    for i in range(len(arr)):
+        if arr[i] == target:
+           cnt += 1
+           if i == len(arr) - 1:
+               break
+           if arr[i + 1] == target:
+               cnt -= 1
+    return cnt
+print(min(solution(0), solution(1)))
