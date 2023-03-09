a31be429be68da34cb5e76e242f4aa4092c9633b
yjglab <yjgdesign@gmail.com>
2023-03-09 13:45:55 +0900

    Add one

--- programmers/lv1/성격 유형 검사하기.js
+++ programmers/lv1/성격 유형 검사하기.js
@@ -1,14 +1,5 @@
 function solution(survey, choices) {
-  let table = {
-    R: 0,
-    T: 0,
-    C: 0,
-    F: 0,
-    J: 0,
-    M: 0,
-    A: 0,
-    N: 0,
-  };
+  let table = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
   for (let i = 0; i < survey.length; i += 1) {
     if (choices[i] <= 3) table[survey[i][0]] += Math.abs(4 - choices[i]);
     else if (choices[i] >= 5) table[survey[i][1]] += Math.abs(4 - choices[i]);
